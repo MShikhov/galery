d19f448d5ae92574c4987403f36b90a7ef3e702f
Misha Shikhov <Mishamoskovsky@gmail.com>
2021-07-01 18:30:47 +0300

    first commit

---
+++ templates/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+	<p>&copy; Все права защищены</p>
+	<a href="https://www.instagram.com/ubermensch_96/" target="_blank">Instagram</a>
+</footer>
+<script src="/js/script.js"></script>
+</body>
+
+</html>
